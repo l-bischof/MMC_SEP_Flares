42b479b2f8491e8d82732d22dbc67cce2607c7aa
l-bischof <louis.bischof@pmodwrc.ch>
2025-02-17 09:20:18 +0100

    Test: Added Test for off by one error

--- Code/tests/test_epd.py
+++ Code/tests/test_epd.py
@@ -12,6 +12,7 @@ class TestEPD(unittest.TestCase):
     def test_nans(self):
         df = pd.read_pickle("./Code/tests/data/fix-2021-04-19.pkl")
         df1 = reduce_data(df, "ept")
+        # There should be no NaN's -> Off by one error
         nans = df1[df1.isna().sum(axis=1) > 5]
         self.assertEqual(len(nans), 0)
 
