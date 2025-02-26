ea0f8923e3412fed2ad2b6f7e38c9ea029a6cf78
l-bischof <louis.bischof@pmodwrc.ch>
2025-02-26 17:34:58 +0100

    Feat: Added Parameter Delta

--- Code/config.py
+++ Code/config.py
@@ -2,7 +2,9 @@ OUTPUT_DIR  = "../Data"
 CACHE_DIR   = "../Cache"
 
 # Format YYYY-MM-DD
-START_DATE  = '2021-05-21'
-END_DATE    = '2021-05-23'
+START_DATE  = '2023-02-23'
+END_DATE    = '2023-02-24'
 
 TIME_RESOLUTION = 300 # In Seconds
+
+DELTA = 10 # Degrees
